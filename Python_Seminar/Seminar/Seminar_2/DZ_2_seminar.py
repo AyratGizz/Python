18616605e68aea9284a0e0667c17f191b9a4ebaa
AyratGizz <gizzatullin-airat@mail.ru>
2022-11-20 20:12:21 +0500

    Calc_end

--- Python_Seminar/Seminar/Seminar_2/DZ_2_seminar.py
+++ Python_Seminar/Seminar/Seminar_2/DZ_2_seminar.py
@@ -1,196 +1,206 @@
-## ------------------------------------------------------
-## №14 Напишите программу, которая принимает на вход вещественное число и показывает 
-## сумму его цифр.
-## Пример:
-## - 6782 -> 23
-## - 0,56 -> 11
+# ------------------------------------------------------
+# №14 Напишите программу, которая принимает на вход вещественное число и показывает
+# сумму его цифр.
+# Пример:
+# - 6782 -> 23
+# - 0,56 -> 11
 
-## Решение прибавлением чисел начиная с последнего методом деления на 10 
-## и взятия остатка (или целочисленного деления) - !!! не работает с дробными...!!!
+# Решение прибавлением чисел начиная с последнего методом деления на 10
+# и взятия остатка (или целочисленного деления) - !!! не работает с дробными...!!!
 
-# number = float(input('Введите чилсло: '))
-# summ = 0
+number = float(input('Введите число: '))
+summ = 0
 
-# while number != 0:
-#     summ += number % 10
-#     number //= 10
-# print(summ)
+while number != 0:
+    summ += number % 10
+    number //= 10
+print(summ)
 
-## Решение задачи через строки
+# Решение задачи через строки
 
-# number = input('Введите число: ')
-# summ = 0
-# for symbol in number:
-#     if symbol != '.' and symbol != '-':
-#         summ += int(symbol)
-# print(summ)
+number = input('Введите число: ')
+summ = 0
+for symbol in number:
+    if symbol != '.' and symbol != '-':
+        summ += int(symbol)
+print(summ)
 
-## Решение через реплайс (сложный алгоритм)
-# number = input('Введите число: ')
-# summ = 0
-# number.replace('.', '')
-# number.replace('-', '')
-# for symbol in number:
-#     if symbol != '.' and symbol != '-':
-#         summ += int(symbol)
-# print(summ)
+# Решение через реплайс (сложный алгоритм)
+number = input('Введите число: ')
+summ = 0
+number.replace('.', '')
+number.replace('-', '')
+for symbol in number:
+    if symbol != '.' and symbol != '-':
+        summ += int(symbol)
+print(summ)
 
-## Решение методом isdigit
-# sum = 0
-# input_num = input('Введите число: ')
+# Решение методом isdigit
+sum = 0
+input_num = input('Введите число: ')
 
-# for a in input_num:
-#     if a.isdigit():  # Проверка на является символ строки числом... И выдает True или Folse
-#         sum += int(a) # Тут если True то прибавляет к sum число которое проверил isdigit
+for a in input_num:
+    if a.isdigit():  # Проверка не является символ строки числом... И выдает True или Folse
+        sum += int(a)  # Тут если True то прибавляет к sum число, которое проверил isdigit
 
-# print(sum)
+print(sum)
 
-##------------------------------------------------------------------
-## № 15 Напишите программу, которая
-## принимает на вход число N и выдает набор произведений чисел от 1 до N.
+# ------------------------------------------------------------------
+# № 15 Напишите программу, которая
+# принимает на вход число N и выдает набор произведений чисел от 1 до N.
 
-# n = int(input('Введите чилос: '))
-# some_list = [1]
-# fact = 1
-# for i in range(2, n+1):
-#     fact *= i
-#     some_list.append(fact)
-# print(some_list)
-
-## ------------
-# def factorial (number, count = 1):
-#     for i in range(1, number + 1):
-#         count *= i
-#     return count
-
-# n = int(input('Введите число: '))
-# print(f'Произведение чисел от 1 до {n} = ', end = '')
-# for i in range(1, n + 1):
-#     if i == n: 
-#         print(f'{factorial(i)}')
-#     else:
-#         print(f'{factorial(i)}', end = ', ')
+n = int(input('Введите силос: '))
+some_list = [1]
+fact = 1
+for i in range(2, n + 1):
+    fact *= i
+    some_list.append(fact)
+print(some_list)
 
 
-## № 16 Задайте список из n чисел 
-## последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
-## Вариант 1------
-# n = int(input('Введите число: '))
-# some_list = []
-# for i in range (1, n+1):
-#     some_list.append((1 + 1/i) ** i)
-# print(sum(some_list))
-
-## Вариант 2------
-# n = int(input('Введите число: '))
-# summ = 0
-
-# for i in range(1, n+1):
-#     summ += (1+1/i) ** i
-# print(summ)
-
-## Вариант переданный в ДЗ мной-----
-# n = int(input('Введите число: '))
-
-# def sequence(n):
-#     return[round((1 + 1 / x)**x, 2) for x in range (1, n + 1)]   
-        
-# print(sequence(n))
-# print(round(sum(sequence(n))))
+# ------------
+def factorial(number, count=1):
+    for i in range(1, number + 1):
+        count *= i
+    return count
 
 
-## 17. НЕ ОБЯЗАТЕЛЬНАЯ ЗАДАЧА
-## Задайте список из N элементов, заполненных числами из промежутка [-N, N].
-## Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+n = int(input('Введите число: '))
+print(f'Произведение чисел от 1 до {n} = ', end='')
+for i in range(1, n + 1):
+    if i == n:
+        print(f'{factorial(i)}')
+    else:
+        print(f'{factorial(i)}', end=', ')
 
-## Решение на семинаре------------------------
+# № 16 Задайте список из n чисел
+# последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
+# Вариант 1------
+n = int(input('Введите число: '))
+some_list = []
+for i in range(1, n + 1):
+    some_list.append((1 + 1 / i) ** i)
+print(sum(some_list))
 
-# from random import *
-# n = int(input('Введите число: '))
-# some_list = []
-# for _ in range(n):
-#     some_list.append(randint(-n, n))
-# print(some_list)
-# with open('file.txt', 'w', encoding = 'utf-8') as f:
-#     for _ in range(randint(1, n)):
-#         f.write(str(randint(0, n-1)) + '\n')
-# fact = 1
-# with open('file.txt', 'r', encoding = 'utf-8') as f:    
-#     f = f.read().splitlines()
-#     for number in f:
-#          fact *= some_list[int(number)]
-# print(fact)
+# Вариант 2------
+n = int(input('Введите число: '))
+summ = 0
 
-## Решение переданное мной --------------------
-# import random
+for i in range(1, n + 1):
+    summ += (1 + 1 / i) ** i
+print(summ)
 
-# def fill_list(n: int) -> list: 
-#     new_list = [random.randint(-n, n)]
-#     for i in range(1, n):
-#         new_list.append(random.randint(-n, n))
-#         i += 1
-#     return new_list
+# Вариант переданный в ДЗ мной-----
+n = int(input('Введите число: '))
 
-# def writing_file(k: int, n: int):
-#    with open('file2_4.txt', 'w') as position:
-#        for i in range(k):
-#            position.write(f'{random.randint(0, n-1)}\n')
 
-# def print_position():
-#    path = 'file2_4.txt'
-#    position = open(path, 'r')
-#    pos_element = []
-#    for line in position:
-#     pos_element.append(int(line))
-#    print(f'Позиции элементов: {pos_element}')
-#    position.close()
-#    return pos_element
+def sequence(n):
+    return [round((1 + 1 / x) ** x, 2) for x in range(1, n + 1)]
 
-# def product_elements(user_list: list, k: int) -> int:
-#    path = 'file2_4.txt'
-#    position = open(path, 'r')
-#    product = 1
-#    for line in position:
-#     product = product * user_list[int(line)]
-#    position.close()
-#    return product
 
-# n = int(input('Количество элементов: N = '))
-# new_list = fill_list(n)
-# k = int(input('Количество множителей: k = '))
-# writing_file(k, n)
-# print(f'Заданный список: {new_list}')
-# print_position()
-# print(f'Произведение элементов на заданных позициях равно {product_elements(new_list, k)}')
+print(sequence(n))
+print(round(sum(sequence(n))))
 
-## № 18 Не обязательная. Реализуйте алгоритм перемешивания списка
+# 17. НЕ ОБЯЗАТЕЛЬНАЯ ЗАДАЧА
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N].
+# Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
 
-# import random
-# some_list = [1, 4, 9, 10]
-# random.shuffle(some_list)
-# print(some_list)
+# Решение на семинаре------------------------
 
-## Вариант 2 --- через алгоритм -----
+from random import *
 
-# import random
-# some_list = [1, 4, 9, 10]
-# for _ in range(random.randint(1, 10)):
-#     i1 = random.randint(0, len(some_list)-1)
-#     i2 = random.randint(0, len(some_list)-1)
-#     some_list[i1], some_list[i2] = some_list[i2], some_list[i1]
-# print(some_list)
+n = int(input('Введите число: '))
+some_list = []
+for _ in range(n):
+    some_list.append(randint(-n, n))
+print(some_list)
+with open('file.txt', 'w', encoding='utf-8') as f:
+    for _ in range(randint(1, n)):
+        f.write(str(randint(0, n - 1)) + '\n')
+fact = 1
+with open('file.txt', 'r', encoding='utf-8') as f:
+    f = f.read().splitlines()
+    for number in f:
+        fact *= some_list[int(number)]
+print(fact)
 
-## Вариант через "время" алгоритмом...--------------------
+# Решение переданное мной --------------------
+import random
 
-# import time
-# now = time.time()
-# print(now)
-# random_number = str(time.time()).split('.') [1]
-# some_list = [1, 4, 9, 10]
-# for _ in range(int(str(time.time()).split('.')[1]) % (10 - 5 + 1) + 5):
-#     i1 = int(str(time.time()).split('.')[1])  % (4-0) + 0
-#     time.sleep(0.00001)
-#     i2 = int(str(time.time()).split('.')[1])  % (4-0) + 0
-#     some_list[i1], some_list[i2] = some_list[i2], some_list[i1]
-# print(some_list)
 
+def fill_list(n: int) -> list:
+    new_list = [random.randint(-n, n)]
+    for i in range(1, n):
+        new_list.append(random.randint(-n, n))
+        i += 1
+    return new_list
+
+
+def writing_file(k: int, n: int):
+    with open('file2_4.txt', 'w') as position:
+        for i in range(k):
+            position.write(f'{random.randint(0, n - 1)}\n')
+
+
+def print_position():
+    path = 'file2_4.txt'
+    position = open(path, 'r')
+    pos_element = []
+    for line in position:
+        pos_element.append(int(line))
+    print(f'Позиции элементов: {pos_element}')
+    position.close()
+    return pos_element
+
+
+def product_elements(user_list: list, k: int) -> int:
+    path = 'file2_4.txt'
+    position = open(path, 'r')
+    product = 1
+    for line in position:
+        product = product * user_list[int(line)]
+    position.close()
+    return product
+
+
+n = int(input('Количество элементов: N = '))
+new_list = fill_list(n)
+k = int(input('Количество множителей: k = '))
+writing_file(k, n)
+print(f'Заданный список: {new_list}')
+print_position()
+print(f'Произведение элементов на заданных позициях равно {product_elements(new_list, k)}')
+
+# № 18 Не обязательная. Реализуйте алгоритм перемешивания списка
+
+import random
+
+some_list = [1, 4, 9, 10]
+random.shuffle(some_list)
+print(some_list)
+
+# Вариант 2 --- через алгоритм -----
+
+import random
+
+some_list = [1, 4, 9, 10]
+for _ in range(random.randint(1, 10)):
+    i1 = random.randint(0, len(some_list) - 1)
+    i2 = random.randint(0, len(some_list) - 1)
+    some_list[i1], some_list[i2] = some_list[i2], some_list[i1]
+print(some_list)
+
+# Вариант через "время" алгоритмом...--------------------
+
+import time
+
+now = time.time()
+print(now)
+random_number = str(time.time()).split('.')[1]
+some_list = [1, 4, 9, 10]
+for _ in range(int(str(time.time()).split('.')[1]) % (10 - 5 + 1) + 5):
+    i1 = int(str(time.time()).split('.')[1]) % (4 - 0) + 0
+    time.sleep(0.00001)
+    i2 = int(str(time.time()).split('.')[1]) % (4 - 0) + 0
+    some_list[i1], some_list[i2] = some_list[i2], some_list[i1]
+print(some_list)
--- Python_Seminar/Seminar/Seminar_4/DZ_4_seminar.py
+++ Python_Seminar/Seminar/Seminar_4/DZ_4_seminar.py
@@ -1,171 +1,171 @@
-## № 30. Вычислить число c заданной точностью d. 
-## Пример:
-## при d = 0.001, π = 3.141  10^(-1)≤d≤10^(-10)
+# № 30. Вычислить число c заданной точностью d.
+# Пример:
+# при d = 0.001, π = 3.141  10^(-1)≤d≤10^(-10)
 
-## --- Решение на семинаре--------------
+# --- Решение на семинаре--------------
 
-# n = float(input('Введите число: '))
-# d = float(input('Введите d: '))
+n = float(input('Введите число: '))
+d = float(input('Введите d: '))
 
-# if d == 1:
-#     print(int(n))
-# else:
-#     d = str(d)
-#     d = d.split('.') # Строка делится по '.' - левая часть [0], правая часть [1]
-#     d = len(d[1]) # Выбираем правую сторону от точки    
-#     print(round(n, d)) # round число n со знаками посля запятой длиной d
+if d == 1:
+    print(int(n))
+else:
+    d = str(d)
+    d = d.split('.')  # Строка делится по '.' - левая часть [0], правая часть [1]
+    d = len(d[1])  # Выбираем правую сторону от точки
+    print(round(n, d))  # round число n со знаками посля запятой длиной d
 
-## -- Сдано мной ДЗ на платформе---
-# from math import pi
+# -- Сдано мной ДЗ на платформе---
+from math import pi
 
-# d =  int(input('Введите число для заданной точности числа Пи:\n'))
-# print(f'число Пи с заданной точностью {d} равно {round(pi, d)}')
+d = int(input('Введите число для заданной точности числа Пи:\n'))
+print(f'число Пи с заданной точностью {d} равно {round(pi, d)}')
 
+# № 31. Задайте натуральное число N. Напишите программу, которая составит
+# список простых множителей числа N.
 
-## № 31. Задайте натуральное число N. Напишите программу, которая составит 
-## список простых множителей числа N.
+# --- Решение на семинаре ----------
 
-## --- Решение на семинаре ----------
+n = int(input('Введите число n: '))
+a = []
+for i in range(2, n + 1):
+    if n % i == 0:
+        for j in range(2, i // 2 + 1):
+            if i % j == 0:
+                break
+        else:
+            a.append(i)
+print(a)
 
-# n = int(input('Введите число n: '))
-# a = []
-# for i in range(2, n + 1):
-#     if n % i == 0:
-#         for j in range(2, i // 2 + 1):
-#             if i % j == 0:
-#                 break
-#         else:
-#             a.append(i)
-# print(a)
+# -- Сдано мной ДЗ на платформе----
+num = int(input('Введите число: '))
+i = 2  # первое простое число
+lst = []
+old = num
+while i <= num:
+    if num % i == 0:
+        lst.append(i)
+        num //= i
+        i = 2
+    else:
+        i += 1
+print(f'Простые множители числа {old} приведены в списке: {lst}')
 
-## -- Сдано мной ДЗ на платформе----
-# num = int(input('Введите число: '))
-# i = 2 # первое простое число
-# lst = []
-# old = num
-# while i <= num:
-#     if num % i == 0:
-#         lst.append(i)
-#         num //= i
-#         i = 2
-#     else:
-#         i += 1
-# print(f'Простые множители числа {old} приведены в списке: {lst}')
+# № 32. Задайте последовательность чисел.
+# Напишите программу, которая выведет список неповторяющихся
+# элементов исходной последовательности.
 
-## № 32. Задайте последовательность чисел. 
-## Напишите программу, которая выведет список неповторяющихся 
-## элементов исходной последовательности.
+# - Решение на семинаре------
 
-## - Решение на семинаре------
+a = [2, 5, 8, 8, 3, 0, 3, 2]
+b = set(a)  # Множетсво - set  - хэш таблица из уникальных элементов списка
+print(b)
+## Можно искать в списке искомое значение
+print(3 in b)  # Находится мгновенно в множестве чем просто проходиться по списку...
 
-# a = [2 ,5, 8, 8, 3, 0, 3, 2]
-# b = set(a) # Множетсво - set  - хэш таблица из уникальных элементов списка
-# print(b)
-# ## Можно искать в списке искомое значение
-# print(3 in b) # Находится мгновенно в множестве чем просто проходиться по списку...
+a = [2, 5, 8, 8, 3, 0, 3, 2]
+b = []
+for i in a:
+    if a.count(i) == 1:
+        b.append(i)
+print(b)
 
-# a = [2 ,5, 8, 8, 3, 0, 3, 2]
-# b = []
-# for i in a:
-#     if a.count(i) == 1:
-#         b.append(i)
-# print(b)
+# Для ускорения можно так
 
-## Для ускорения можно так
+a = [2, 5, 8, 8, 3, 0, 3, 2]
+b = []
+for i in a:
+    count = 0
+    for j in a:
+        if i == j:
+            count += 1
+        if count == 2:
+            break
+    if count == 1:
+        print(i)
 
-# a = [2 ,5, 8, 8, 3, 0, 3, 2]
-# b = []
-# for i in a:
-#     count = 0
-#     for j in a:
-#         if i  == j:
-#             count += 1
-#         if count == 2:
-#             break
-#     if count == 1:
-#         print(i)
-   
+# -- Сдан омной ДЗ на платформе---
+lst = list(map(int, input('Введите числа через пробел:\n').split()))
+print(f'Исходный список: {lst}')
+new_lst = []
+[new_lst.append(i) for i in lst if i not in new_lst]
+print(f'Список из неповторяющихся элементов: {new_lst}')
 
-## -- Сдан омной ДЗ на платформе---
-# lst = list(map(int, input('Введите числа через пробел:\n').split()))
-# print(f'Исходный список: {lst}')
-# new_lst = []
-# [new_lst.append(i) for i in lst if i not in new_lst]
-# print(f'Список из неповторяющихся элементов: {new_lst}')
+# № 33. Задана натуральная степень k. Сформировать случайным образом список коэффициентов
+# (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+# Пример: k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x²
 
+# -- Решение на семинаре----------
 
-## № 33. Задана натуральная степень k. Сформировать случайным образом список коэффициентов 
-## (значения от 0 до 100) многочлена и записать в файл многочлен степени k.    
-## Пример: k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x²
+import random
 
-## -- Решение на семинаре----------
+some_dict = {0: '\u2070',
+             1: '\u00B9',
+             2: '\u00B2',
+             3: '\u00B3',
+             4: '\u2074',
+             5: '\u2075',
+             6: '\u2076',
+             7: '\u2077',
+             8: '\u2078',
+             9: '\u2079'}
 
-# import random
+k = int(input('Введите натуральную степень k: '))
 
-# some_dict = {0: '\u2070',
-#            1: '\u00B9',
-#            2: '\u00B2',
-#            3: '\u00B3',
-#            4: '\u2074',
-#            5: '\u2075',
-#            6: '\u2076',
-#            7: '\u2077',
-#            8: '\u2078',
-#            9: '\u2079'}
+coef = [random.randint(0, 100) for _ in range(k + 1)]
+print(coef)
+with open('file.txt', 'w', encoding='utf-8') as m:
+    for i in range(len(coef)):
+        if k - i != 1 and k - i != 0:
+            m.write(f'{coef[i]}x{some_dict[k - i]} + ')
+        elif k - i == 1:
+            m.write(f'{coef[i]}x + ')
+        elif k - i == 0:
+            m.write(f'{coef[i]} = 0')
 
-# k = int(input('Введите натуральную степень k: '))
+# -- Сдано мной ДЗ на платформе-----------
+from random import randint
+import itertools
 
-# coef = [random.randint(0, 100) for _ in range(k+1)]
-# print(coef)
-# with open('file.txt', 'w', encoding='utf-8') as m:
-#     for i in range(len(coef)):
-#         if k - i != 1 and k - i !=0:
-#             m.write(f'{coef[i]}x{some_dict[k - i]} + ')
-#         elif k - i == 1:
-#             m.write(f'{coef[i]}x + ')
-#         elif k - i == 0:
-#             m.write(f'{coef[i]} = 0')
-
-    
-## -- Сдано мной ДЗ на платформе-----------
-# from random import randint
-# import itertools
-
-# k = randint(2, 7)
-
-# def get_ratios(k):
-#     ratios = [randint(0, 10) for i in range (k + 1)]
-#     while ratios[0] == 0:
-#         ratios[0] = randint(1, 10) 
-#     return ratios
-
-# def get_polynomial(k, ratios):
-#     var = ['*x^']*(k-1) + ['*x']
-#     polynomial = [[a, b, c] for a, b, c  in itertools.zip_longest(ratios, var, range(k, 1, -1), fillvalue = '') if a !=0]
-#     for x in polynomial:
-#         x.append(' + ')
-#     polynomial = list(itertools.chain(*polynomial))
-#     polynomial[-1] = ' = 0'
-#     return '.join(map(str, polynomial)).replace(' 1*x',' x')
+k = randint(2, 7)
 
 
-# ratios = get_ratios(k)
-# polynom1 = get_polynomial(k, ratios)
-# print(polynom1)
+def get_ratios(k):
+    ratios = [randint(0, 10) for i in range(k + 1)]
+    while ratios[0] == 0:
+        ratios[0] = randint(1, 10)
+    return ratios
 
-# with open('Polynomial.txt', 'w') as data:
-#     data.write(polynom1)
+
+def get_polynomial(k, ratios):
+    var = ['*x^'] * (k - 1) + ['*x']
+    polynomial = [[a, b, c] for a, b, c in itertools.zip_longest(ratios, var, range(k, 1, -1), fillvalue='') if a != 0]
+    for x in polynomial:
+        x.append(' + ')
+    polynomial = list(itertools.chain(*polynomial))
+    polynomial[-1] = ' = 0'
+    return '.join(map(str, polynomial)).replace('
+    1 * x
+    ','
+    x
+    ')
 
 
-## № 35. Даны два файла, в каждом из которых находится запись многочлена. 
-## Задача - сформировать файл, содержащий сумму многочленов.
+ratios = get_ratios(k)
+polynom1 = get_polynomial(k, ratios)
+print(polynom1)
 
-# k = randint(2, 5)
+with open('Polynomial.txt', 'w') as data:
+    data.write(polynom1)
 
-# ratios = get_ratios(k) 
-# polynom2 = get_polynomial(k, ratios)
-# print(polynom2)
+# № 35. Даны два файла, в каждом из которых находится запись многочлена.
+# Задача - сформировать файл, содержащий сумму многочленов.
 
-# with open('Polynomial2.txt', 'w') as data:
-#     data.write(polynom2)
+k = randint(2, 5)
 
+ratios = get_ratios(k)
+polynom2 = get_polynomial(k, ratios)
+print(polynom2)
+
+with open('Polynomial2.txt', 'w') as data:
+    data.write(polynom2)
